f4481ad2e73a783b8e5bbdbfb9e0e18d1cc8c04f
mqbaka\mqbaka <mqbaka@gmail.com>
2024-11-07 23:12:28 +0300

    Added a contentPane

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,7 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Test {
 	public static void main(String[] args){
@@ -9,7 +12,12 @@ public class Test {
 		fenetre.setLocationRelativeTo(null);//center the window
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JPanel panel= new JPanel();
+		panel.setBackground(Color.GREEN);
+		fenetre.setContentPane(panel);
+		
 		fenetre.setVisible(true);//required if the window ->
 		//is supposed to be visible
+		
 	}
 }
